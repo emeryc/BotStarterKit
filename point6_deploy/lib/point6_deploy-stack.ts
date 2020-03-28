9f1300e8d3c40ecf64aefb950eb7c331d16b6193
Chris Emery <chris@menagerie.house>
2020-03-28 14:18:11 -0700

    Maybe missed something?

--- point6_deploy/lib/point6_deploy-stack.ts
+++ point6_deploy/lib/point6_deploy-stack.ts
@@ -4,16 +4,20 @@ import * as sns from "@aws-cdk/aws-sns";
 import * as subs from "@aws-cdk/aws-sns-subscriptions";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as iam from "@aws-cdk/aws-iam";
+import * as secretManager from "@aws-cdk/aws-secretsmanager"
 
 export class Point6DeployStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const signingSecret = new secretManager.Secret(this, "SlackSigningSecret")
 
     const slackIncomingHandler = new lambda.Function(this, "BigHeroPoint6", {
       runtime: lambda.Runtime.PROVIDED,
       code: lambda.Code.fromAsset("../target/x86_64-unknown-linux-musl/release/slack_incoming_handler.zip"),
       handler: "ignored"
     })
+    signingSecret.grantRead(slackIncomingHandler)
 
     slackIncomingHandler.role?.addToPolicy(new iam.PolicyStatement({
       effect: iam.Effect.ALLOW,
@@ -39,11 +43,14 @@ export class Point6DeployStack extends cdk.Stack {
 
     messages.grantPublish(slackIncomingHandler)
 
+    const clientSecret = new secretManager.Secret(this, "SlackClientSecret")
+
     const bigHeroEcho = new lambda.Function(this, "BigHeroEcho", {
       runtime: lambda.Runtime.PROVIDED,
       code: lambda.Code.fromAsset("../target/x86_64-unknown-linux-musl/release/big_hero_echo.zip"),
       handler: "ignored"
     })
+    clientSecret.grantRead(bigHeroEcho)
 
     messages.addSubscription(new subs.LambdaSubscription(bigHeroEcho, {}));
 
